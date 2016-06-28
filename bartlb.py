d3678c51e53ba6d1a9f3eaaeecd212924444bdfd
Jake Kara <jake@jakekara.com>
2016-06-28 08:21:40 -0400

    Added no_index_mode and fill_na_mode options

--- bartlb.py
+++ bartlb.py
@@ -6,9 +6,14 @@ class scribbler:
     def read(self):
         print "Reading " + self.csv_file
         # try:
-        self.df = pd.read_csv(self.csv_file)
+        if self.no_index_mode:
+            self.df = pd.read_csv(self.csv_file)
+        else:
+            self.df = pd.read_csv(self.csv_file, index_col=False)
+        if self.fill_na_mode:
+            self.df = self.df.fillna("")
         print "Successfully read dataframe"
-        print self.df.dtypes
+        # print self.df.dtypes
         # except:
         #     "Failed to read csv '" + self.csv_file + "'"
         print "Done"
@@ -21,16 +26,18 @@ class scribbler:
             self.db_info["password"] +"@"+ self.db_info["hostname"] + "/" +\
             self.db_info["database"])
 
-        try:
-            self.df.to_sql(self.table_name,
-                           engine,
-                           if_exists=self.if_exists_mode,
-                           dtype=self.custom_data_types)
-        except:
-            print "Unable to create table"
+        # try:
+        self.df.to_sql(self.table_name,
+                       engine,
+                       if_exists=self.if_exists_mode,
+                       dtype=self.custom_data_types)
+        # except e:
+        #     print "Unable to create table", e
 
     # if_exists = "fail" | "replace" | "append"  
-    def __init__(self, db_conf, table, infile, if_exists="fail", custom_dtypes={}):
+    def __init__(self, db_conf, table, infile,
+                 if_exists="fail",custom_dtypes={},fill_na=False,no_index=False):
+        
         print "Making new object"
 
         self.db_info = db_conf
@@ -38,4 +45,6 @@ class scribbler:
         self.table_name = table
         self.if_exists_mode = if_exists
         self.custom_data_types = custom_dtypes
+        self.fill_na_mode = fill_na
+        self.no_index_mode = no_index
         
